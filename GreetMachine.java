ac62830201b63b4f7ec43f7074f81bda0f478a2a
Darryl Terrell <dterrel2@xula.edu>
2022-08-31 14:43:33 +0000

    added GreetMachine object

---
+++ GreetMachine.java
@@ -0,0 +1,16 @@
+public class GreetMachine{
+    private String name;
+
+    public GreetMachine() {
+        name = "Earthlings";
+
+    }
+
+    public GreetMachine(String name_in){
+        name = name_in;
+    }
+
+    public String toString(){
+        return "Greetings " + name + "!";
+    }
+}
--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,9 @@
 public class Greeter {
     public static void main(String[] args) {
+
+        GreetMachine gm = new GreetMachine();
+        System.out.println(gm);
+
         // Condition ? value when condition is true : when value is false
         String name = args.length > 0 ? args[0] : "Earthlings";
 
