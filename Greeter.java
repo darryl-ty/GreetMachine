1c910a200d39704c762fd1d688961a5bec9550b1
Darryl Terrell <dterrel2@xula.edu>
2022-09-02 14:09:26 +0000

    changed default to 'people of earth'

--- Greeter.java
+++ Greeter.java
@@ -1,9 +1,13 @@
 public class Greeter {
     public static void main(String[] args) {
+
+        String DEFAULT_NAME = "people of Earth";
+        
         // Condition ? value when condition is true : when value is false
-        String name = args.length > 0 ? args[0] : "Earthlings";
+        String name = args.length > 0 ? args[0] : DEFAULT_NAME;
 
         System.out.printf("Greetings %s!\n", name);
+        System.out.println("Have a nice day.")
 
     }
 }
